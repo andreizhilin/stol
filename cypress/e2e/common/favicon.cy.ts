2a0c9d0929e24a607063a8bebbcd01f77d02a4b6
Andrei Zhilin <andrei@zhilin.app>
2022-07-18 12:36:00 +0300

    favicon should exist

---
+++ cypress/e2e/common/favicon.cy.ts
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe('favicon', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('apple-touch-icon should exist', () => {
+    cy.document()
+      .its('head')
+      .find('link[rel="apple-touch-icon"]')
+      .should('have.attr', 'href')
+      .should('eq', '/apple-touch-icon.png');
+  });
+
+  it('32x32 icon should exist', () => {
+    cy.document()
+      .its('head')
+      .find('link[rel="icon"][sizes="32x32"]')
+      .should('have.attr', 'href')
+      .should('eq', '/favicon-32x32.png');
+  });
+
+  it('16x16 icon should exist', () => {
+    cy.document()
+      .its('head')
+      .find('link[rel="icon"][sizes="16x16"]')
+      .should('have.attr', 'href')
+      .should('eq', '/favicon-16x16.png');
+  });
+
+  it('manifest should exist', () => {
+    cy.document()
+      .its('head')
+      .find('link[rel="manifest"]')
+      .should('have.attr', 'href')
+      .should('eq', '/site.webmanifest');
+  });
+
+  it('mask-icon should exist', () => {
+    cy.document()
+      .its('head')
+      .find('link[rel="mask-icon"]')
+      .should('have.attr', 'href')
+      .should('eq', '/safari-pinned-tab.svg');
+  });
+});
