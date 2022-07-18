6e6a2a1c38e16630ec4b1a6d78b748284b457ffd
Andrei Zhilin <andrei@zhilin.app>
2022-07-18 12:36:39 +0300

    change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Arial', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
